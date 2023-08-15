bf09283f167b98972be154d14b2408ec112bfe0d
Paulo Vinicius Cristaldo A <102568081+Paulo-vca@users.noreply.github.com>
2023-08-14 20:19:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function soma() {
+
+    var v1 = document.getElementById("txtV1").value;
+    var v2 = document.getElementById("txtV2").value;
+
+    var result = parseFloat(v1) + parseFloat(v2);
+
+    // var resul = document.getElementById('txtReseult').value;
+    document.getElementById("txtV3").value = result;
+
+}
+
+function titulo() {
+
+    document.getElementById("titulo01").innerHTML = "Eduardo"
+    
+}
+
+    document.getElementById("txtV2")
